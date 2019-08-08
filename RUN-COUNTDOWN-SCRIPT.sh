d1d4727ce00f648b84f780a56c7f15b02874bffc
Poporacer <46263877+Poporacer@users.noreply.github.com>
2019-08-08 11:54:49 -0600

    Update RUN-COUNTDOWN-SCRIPT.sh

--- RUN-COUNTDOWN-SCRIPT.sh
+++ RUN-COUNTDOWN-SCRIPT.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #Script to run countdown from playlist
+#Created by EventDate
 #
-#
-/usr/bin/php /home/fpp/media/plugins/EventDate/runEventDate.php
+/usr/bin/php /home/fpp/media/plugins/FPP-Plugin-EventDate/runEventDate.php
