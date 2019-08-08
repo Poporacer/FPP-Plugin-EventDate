8a6b3264991463f35613960acb9680500da531df
Poporacer <46263877+Poporacer@users.noreply.github.com>
2019-08-08 11:55:36 -0600

    Update fpp_install.sh

--- fpp_install.sh
+++ fpp_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pushd $(dirname $(which $0))
 target_PWD=$(readlink -f .)
-exec /opt/fpp/scripts/update_plugin ${target_PWD##*/}
+/opt/fpp/scripts/update_plugin ${target_PWD##*/}
 /bin/cp RUN-COUNTDOWN-SCRIPT.sh /home/fpp/media/scripts
 popd
