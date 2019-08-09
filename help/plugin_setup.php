28386fb60d638c741b50c6af67088e0368793869
Poporacer <46263877+Poporacer@users.noreply.github.com>
2019-08-09 15:59:28 -0600

    Create plugin_setup.php
    Adding help file for the configuration page

---
+++ help/plugin_setup.php
@@ -0,0 +1,4 @@
+<h2>The ENABLE PLUGIN box needs to be checked for the plugin to be activated. This plugin also requires Message Queue to Matrix Overlay, Message Queue Aggregator for Plugins, and Matrix Tools to be installed and enabled to work.<br>
+To get the current countdown time, you need to run the RUN-COUNTDOWN-SCRIPT.sh and this will store the countdown time on the Message Queue Aggregator until you run the RUN-MATRIX.sh script.<br>
+If you check the Immediately output to Matrix (Run MATRIX plugin) box then it will display your countdown immediately and you don't need to run the RUN-MATRIX.sh script<br>
+The default IP address of 127.0.0.1 should be entered in the MATRIX Message Plugin Location unless you are running an advanced configuration<h2>
